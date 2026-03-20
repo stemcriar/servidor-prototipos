ae0334af6db539b715aafd3e1389e99ca54892f6
Célio Benjamim <celiolimabenjamim@gmail.com>
2026-03-20 12:06:10 -0400

    first commit

---
+++ public/javascripts/dashboard_script.js
@@ -0,0 +1,143 @@
+var connection = new WebSocket("ws://" + location.hostname + ":1801/", [
+  "arduino",
+]);
+
+connection.onopen = function () {
+  connection.send(startPage());
+};
+
+connection.onerror = function (error) {
+  console.log("WebSocket Error ", error);
+};
+
+connection.onmessage = function (e) {
+  console.log("Server: ", e.data);
+  const prototypeData = JSON.parse(e.data);
+
+  if (prototypeData.from == "carSpeed") {
+    handleCartSpeed(prototypeData.state);
+  } else {
+    // States: atM1(Gif1), toM2(Gif2), atM2(Gif3), nearM2(Gif4), undefined(cartSpeed)
+    try {
+      switch (prototypeData.state) {
+        case "atM2":
+          handleAtM1State();
+          break;
+        case "onCar":
+          handleToM2State();
+          break;
+        case "atM1":
+          handleAtM2State();
+          break;
+        case "nearM2":
+          handleNearM2State();
+          break;
+        default:
+          throw new Error("Invalid State");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+connection.onclose = function () {
+  console.log("WebSocket connection closed");
+};
+
+function handleAtM1State() {
+  playAnimationGif("gif1");
+  renderStateOfArm1("on");
+  renderStateOfRunningMachine("off");
+}
+
+function handleToM2State() {
+  playAnimationGif("gif2");
+  renderStateOfArm1("off");
+}
+
+function handleAtM2State() {
+  playAnimationGif("gif3");
+  renderStateOfArm2("on");
+}
+
+function handleNearM2State() {
+  playAnimationGif("gif4");
+  renderStateOfRunningMachine("on");
+  renderStateOfArm2("off");
+}
+
+function handleCartSpeed(speed) {
+  renderCartSpeed(speed);
+}
+
+function playAnimationGif(gifName) {
+  const prototypeGif = document.querySelector("#prototype-gif");
+  const path = "./images/dashboard/";
+
+  prototypeGif.src = `${path}${gifName}.gif`;
+}
+
+function changePrototypeCardState(element, state) {
+  if (state === "on") {
+    element.innerText = "On";
+    element.parentElement.classList.add("on");
+  } else {
+    element.innerText = "Off";
+    element.parentElement.classList.remove("on");
+  }
+}
+
+function renderStateOfRunningMachine(state) {
+  const runningMachine = document.querySelector("#running-machine");
+  changePrototypeCardState(runningMachine, state);
+}
+
+function renderStateOfArm1(state) {
+  const arm1 = document.querySelector("#arm1");
+  changePrototypeCardState(arm1, state);
+}
+
+function renderStateOfArm2(state) {
+  const arm2 = document.querySelector("#arm2");
+  changePrototypeCardState(arm2, state);
+}
+
+function renderCartSpeed(speed) {
+  const cartSpeed = document.querySelector("#cart-speed");
+  cartSpeed.innerText = speed;
+
+  if (speed !== 0) {
+    cartSpeed.parentElement.classList.add("on");
+  } else {
+    cartSpeed.parentElement.classList.remove("on");
+  }
+}
+
+function startPage() {
+  let id = getCookie("esp_id");
+  let robot = getCookie("robot");
+  startInfo = {
+    start: "page_on",
+    to: id,
+    meta: robot,
+  };
+  startInfoJson = JSON.stringify(startInfo);
+  return startInfoJson;
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
---
+++ public/javascripts/index_script.js
@@ -0,0 +1,99 @@
+window.setInterval(searchESPs, 1000);
+
+function searchESPs() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      esp_list = JSON.parse(this.responseText);
+        // esp_list = [
+        //   { id: "001", type: "sistema" },
+        //   { id: "002", type: "manipulador" },
+        //   { id: "003", type: "carrinho" },
+        // ]; // this line
+
+      const mainDiv = document.getElementById("esps_ativos");
+      let htmlContainer = ``;
+
+      if (mainDiv.hasChildNodes()) {
+        while (mainDiv.hasChildNodes()) {
+          mainDiv.removeChild(mainDiv.firstChild);
+        }
+        // mainDiv.innerHTML = ``;
+      }
+
+      if (esp_list.length > 0) {
+        const systemEsp = esp_list.find((esp) => esp.type === "sistema");
+
+        if (systemEsp) {
+          if (!systemEsp.taken) {
+            htmlContainer += 
+            `<div class="container-monitoring-button">
+              <a class="monitoring-button"
+                onclick="
+                startRobot(${systemEsp.id}, 'dashboard');
+                "
+              >
+                <p>Monitoramento<span>ID: ${systemEsp.id}</span></p>
+              </a>
+            </div>`;
+          }
+        };
+
+        esp_list.forEach((esp) => {
+          if(!esp.taken && esp.type === "manipulador"){
+            htmlContainer += `
+              <div class="container-img">
+                <img src="./images/index/robot-arm.png" class="robot-img">
+              </div>
+
+              <div class="container-arm-car-button">
+                <a
+                  class="arm-button"
+                  onclick="
+                    startRobot(${esp.id}, 'manipulador');
+                  "
+                >
+                <img src="./images/index/robotic-arm.svg" />
+                  <p>Braço robótico<span>ID: ${esp.id}</span></p>
+                </a>
+              </div>
+              
+            `;
+          }
+        });
+
+        esp_list.forEach((esp) => {
+          if (!esp.taken && esp.type === "carrinho") {
+            htmlContainer += `
+              <div class="container-img">
+                <img src="./images/carrinho/Chassi_II.png" class="car-img">
+              </div>
+              
+              <div class="container-arm-car-button">
+                <a
+                  class="car-button"
+                  onclick="
+                    startRobot(${esp.id}, 'carrinho');
+                  "
+                >
+                  <img src="./images/index/car.svg" />
+                  <p>Carrinho<span>ID: ${esp.id}</span></p>
+                </a>
+              </div>`;
+          }
+        });
+
+        mainDiv.innerHTML = htmlContainer;
+      }
+    }
+  };
+  xhttp.open("GET", "/getESPs", true);
+  xhttp.send();
+}
+
+function startRobot(esp_id, robot) {
+  console.log("start");
+  document.cookie = "esp_id" + "=" + esp_id;
+  document.cookie = "robot" + "=" + robot;
+  location.href = "/" + robot;
+}
